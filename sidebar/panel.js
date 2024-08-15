4d80761992847bc89bd7a386152e74ee2abcc9a0
coder3337 <coder3337@gmail.com>
2024-08-15 23:58:29 +0200

    milestone(ADDS FEAT)email input send -> proper storage -> cleaned up -> major functions work

--- sidebar/panel.js
+++ sidebar/panel.js
@@ -1,185 +1,225 @@
 let myWindowId;
-const contentBox = document.querySelector("#content");
-
-/* display previously-saved stored notes on startup */
-
-/* initialize();
-
-function initialize() {
-  let gettingAllStorageItems = browser.storage.local.get(null);
-  gettingAllStorageItems.then((results) => {
-    let noteKeys = Object.keys(results);
-    for (let noteKey of noteKeys) {
-      let curValue = results[noteKey];
-      displayNote(noteKey, curValue);
-    }
-  }, onError);
-} */
-
-/*
-Make the content box editable as soon as the user mouses over the sidebar.
-*/
-window.addEventListener("mouseover", () => {
-  contentBox.setAttribute("contenteditable", true);
-});
-
-/*
-When the user mouses out, save the current contents of the box.
-*/
-window.addEventListener("mouseout", () => {
-  contentBox.setAttribute("contenteditable", false);
-  // Save the current contents of the box to local storage.
-  browser.tabs.query({ windowId: myWindowId, active: true }).then((tabs) => {
-    //console.log({ windowId: myWindowId, active: true });
-    let textBox = document.getElementById("content").value;
-    console.log(textBox);
-    /*     contentBox.textContent;
-     */ localStorage.setItem("Text box", textBox);
-  });
-});
-/*
-Update the sidebar's content.
-
-1) Get the active tab in this sidebar's window.
-2) Get its stored content.
-3) Put it in the content box.
-*/
-/* function updateContent() {
-  browser.tabs
-    .query({ windowId: myWindowId, active: true })
-    .then((tabs) => {
-      return browser.storage.local.get(tabs[0].url);
-    })
-    .then((storedInfo) => {
-      contentBox.textContent = storedInfo[Object.keys(storedInfo)[0]];
-    });
-} */
-
-/*
-Update content when a new tab becomes active.
-*/
-//browser.tabs.onActivated.addListener(updateContent);
-
-/*
-Update content when a new page is loaded into a tab.
-*/
-//browser.tabs.onUpdated.addListener(updateContent);
-
-/*
-When the sidebar loads, get the ID of its window,
-and update its content.
-*/
-/* browser.windows.getCurrent({ populate: true }).then((windowInfo) => {
-  myWindowId = windowInfo.id;
-  updateContent();
-}); */
-
-function logTabs(tabs) {
-  //console.log(tabs[0]);
-  let id = tabs[0].id;
-  let host = tabs[0].url;
-  let domain = new URL(host).hostname.replace("www.", "");
-  let title = tabs[0].title;
-  let url = tabs[0].url;
-  let faviconUrl = tabs[0].favIconUrl;
-
-  const btn = document.querySelector("#addTabButton");
-  btn.addEventListener("click", addItem);
-
-  function addItem() {
-    const ul = document.querySelector(".tabsList");
-    const li = document.createElement("li");
-    const titleSpan = document.createElement("a");
-    const hostnameSpan = document.createElement("a");
-    const closeBtn = document.createElement("span");
-
-    const imgLink = document.createElement("a");
-    const img = document.createElement("img");
-
-    ul.appendChild(li);
-    li.className = "tab";
-
-    li.appendChild(imgLink);
-    imgLink.appendChild(img);
-
-    li.appendChild(closeBtn);
-    li.appendChild(titleSpan);
-    li.appendChild(hostnameSpan);
-    //url = str.substring(0, 10);
-    /*   urlSpan.className = "url";
-    urlSpan.textContent = url; */
-
-    titleSpan.className = "title";
-    titleSpan.href = url;
-    titleSpan.textContent = title;
-
-    hostnameSpan.className = "url";
-    hostnameSpan.href = url;
-    hostnameSpan.textContent = domain;
-
-    imgLink.href = url;
-
-    img.className = "tabIcon";
-    img.href = url;
-    img.src = faviconUrl;
-
-    closeBtn.className = "close";
-    closeBtn.textContent = "x";
-
-    // save to local storage
-    let timestamp = new Date().toLocaleString();
-    let uniqueId = id++;
-    /*  source.forEach((item, i) => {
-      item.id = i + 1;
-    }); */
-    const tabGroup = [];
-    tabGroup.push(uniqueId);
-    tabGroup.push(timestamp);
-    tabGroup.push(id);
-    tabGroup.push(title);
-    tabGroup.push(url);
-    tabGroup.push(faviconUrl);
-
-    localStorage.setItem(uniqueId, tabGroup);
-
-    // Add event listener to the close button after it's added to the DOM
-    closeBtn.addEventListener("click", function (e) {
-      e.preventDefault();
-      this.parentNode.remove(); // This will remove the li element
-      localStorage.removeItem(uniqueId);
-    });
-
-    // remove all bookmark items from sidebar
-    const resetBtn = document.querySelector(".resetButton");
-    resetBtn.addEventListener("click", function () {
-      // clear the local storage
-      localStorage.clear();
-
-      const li = document.getElementsByClassName("tab");
-      // console.log(li);
-      while (li.length > 0) {
-        // New JS remove Function
-        li[0].remove();
-        // Safer cross browser
-        // Has to traverse by going up to the parent and removing the child
-        // Which is itself, the li element.
-        li[0].parentNode.removeChild(li[0]);
-      }
-      // tab.remove();
-      //this.parentNode.remove(); // This will remove the li element
-    });
-  }
-}
-
-function onError(error) {
-  console.error(`Error: ${error}`);
-}
-
-browser.tabs
-  .query({ currentWindow: true, active: true })
-  .then(logTabs, onError);
+const contentBox = document.getElementById("content");
+contentBox.placeholder = "Subject goes here..\n\nBody content here..";
 
 browser.windows.getCurrent({ populate: true }).then((windowInfo) => {
   myWindowId = windowInfo.id;
   //console.log(myWindowId);
+});
+
+initialize();
+
+function initialize() {
+  // get notes from storage and update text box
+  let storedNotes = localStorage.getItem("scratchPadText") || [];
+  contentBox.value = storedNotes;
+
+  let container = document.getElementById("content");
+
+  let emailField = document.createElement("input");
+  // emailField.required = true;
+  // emailField.setAttribute("required", "");
+  emailField.contentEditable = false;
+  //emailField.required = true;
+  emailField.style.width = "230px";
+  emailField.type = "email";
+  emailField.id = "email";
+  let storedEmail = localStorage.getItem("email");
+
+  if (!storedEmail) {
+    emailField.placeholder = "enter an email or clickup board!";
+    // alert("Please fill out the input field!");
+  } else if (storedEmail) {
+    emailField.value = storedEmail;
+  }
+
+  let saveButton = document.createElement("button");
+  saveButton.type = "submit";
+  saveButton.className = "sendButton";
+  saveButton.style.width = "100px";
+  saveButton.textContent = "Send";
+  container.before(emailField);
+  container.before(saveButton);
+  saveButton.addEventListener("click", function () {
+    let email = emailField.value;
+    localStorage.setItem("email", email);
+    const subject = storedNotes.slice(0, storedNotes.indexOf("\n"));
+    const notes = storedNotes.slice(storedNotes.indexOf("\n"))
+    // create a new tab and send an email with the text
+    /*  browser.tabs.create({
+      url: "https://app.clickup.com/login",
+    }); */
+        browser.tabs.create({
+          url: `mailto:${email}?subject=${subject}&body=${notes}`,
+        });
+  });
+  // Make the content box editable as soon as the user mouses over the sidebar.
+  const mail = document.getElementById("email");
+  mail.addEventListener("mouseover", () => {
+    emailField.style.width = "250px";
+
+    console.log("mail");
+    emailField.setAttribute("contenteditable", true);
+  });
+  //When the user mouses out, save the current contents of the box.
+  mail.addEventListener("mouseout", () => {
+    emailField.setAttribute("contenteditable", false);
+    emailField.style.width = "230px";
+
+    let email = document.getElementById("email").value;
+    // Save the current contents of the box to local storage.
+    localStorage.setItem("email", email);
+  });
+  //console.log(sendToLink.textContent);
+
+  // get bookmarks from storage
+  let existingTabs = JSON.parse(localStorage.getItem("Tab")) || [];
+  if (existingTabs.length > 0) {
+    //console.log("bookmarks", existingTabs);
+    existingTabs.forEach((tab) => {
+      addTabToUI(tab);
+    });
+  } else {
+    console.log("checked for existing items...nothing");
+  }
+
+  // Make the content box editable as soon as the user mouses over the sidebar.
+  window.addEventListener("mouseover", () => {
+    contentBox.setAttribute("contenteditable", true);
+  });
+  //When the user mouses out, save the current contents of the box.
+  window.addEventListener("mouseout", () => {
+    contentBox.setAttribute("contenteditable", false);
+    let scratchPadText = document.getElementById("content").value;
+    // Save the current contents of the box to local storage.
+    localStorage.setItem("scratchPadText", scratchPadText);
+  });
+}
+
+btn = document.querySelector("#addTabButton");
+//console.log("btn", btn);
+btn.addEventListener("click", function () {
+  browser.tabs
+    .query({ currentWindow: true, active: true })
+    .then(saveItems, onError);
+  function onError(error) {
+    console.error(`Error: ${error}`);
+  }
+});
+
+// add new items to storage
+function saveItems(tabs) {
+  console.log("tabsq", tabs);
+
+  // Check if tabs are valid
+  if (tabs.length === 0) return;
+
+  let id = tabs[0].id;
+  let host = tabs[0].url;
+  let title = tabs[0].title;
+  let url = tabs[0].url;
+  let favIconUrl = tabs[0].favIconUrl;
+
+  // Retrieve the existing array from localStorage, or initialize it as an empty array if it doesn't exist
+  let existingTabs = JSON.parse(localStorage.getItem("Tab")) || [];
+  // Check if the tab already exists in localStorage
+  const tabExists = existingTabs.some((tab) => tab.id === id);
+  let container = document.getElementById("content");
+  if (tabExists) {
+    /*     emailErrorMsg.style.visibility = "hidden";
+     */ //emailErrorMsg.removeChild(emailErrorMsg);
+    let emailErrorMsg = document.createElement("div");
+    emailErrorMsg.classList = "error";
+    setTimeout(() => {
+      const el = document.querySelector(".error");
+      el.remove();
+    }, "500");
+
+    emailErrorMsg.innerHTML = "<p>This tab is already in your list!</p>";
+
+    container.before(emailErrorMsg);
+    //console.log("Tab already exists, not adding duplicate.");
+    return;
+  }
+
+  // Create the new tab object
+  const newTab = {
+    id: id,
+    timestamp: new Date().toLocaleString(),
+    title: title,
+    url: url,
+    favIconUrl: favIconUrl,
+  };
+
+  // Add the new tab to the existing array
+  existingTabs.push(newTab);
+
+  // Save the updated array back to localStorage
+  localStorage.setItem("Tab", JSON.stringify(existingTabs));
+
+  // Add the new tab to the UI
+  addTabToUI(newTab);
+}
+
+function addTabToUI(tab) {
+  const ul = document.querySelector(".tabsList");
+  const li = document.createElement("li");
+  const titleSpan = document.createElement("a");
+  const hostnameSpan = document.createElement("a");
+  const closeBtn = document.createElement("span");
+
+  const imgLink = document.createElement("a");
+  const img = document.createElement("img");
+
+  ul.appendChild(li);
+  li.className = "tab";
+
+  li.appendChild(imgLink);
+  imgLink.appendChild(img);
+
+  li.appendChild(closeBtn);
+  li.appendChild(titleSpan);
+  li.appendChild(hostnameSpan);
+
+  titleSpan.className = "title";
+  titleSpan.href = tab.url;
+  titleSpan.textContent = tab.title;
+
+  hostnameSpan.className = "url";
+  hostnameSpan.href = tab.url;
+  hostnameSpan.textContent = tab.url;
+
+  imgLink.href = tab.url;
+
+  img.className = "tabIcon";
+  img.src = tab.favIconUrl;
+
+  closeBtn.className = "close";
+  closeBtn.textContent = "x";
+
+  closeBtn.addEventListener("click", function () {
+    this.parentNode.remove(); // This will remove the li element
+    removeTabFromStorage(tab.id);
+  });
+}
+
+function removeTabFromStorage(id) {
+  let existingTabs = JSON.parse(localStorage.getItem("Tab")) || [];
+  existingTabs = existingTabs.filter((tab) => tab.id !== id);
+  localStorage.setItem("Tab", JSON.stringify(existingTabs));
+}
+
+// remove all bookmark items from sidebar
+const resetBtn = document.querySelector(".resetButton");
+resetBtn.addEventListener("click", function () {
+  // clear the local storage
+  localStorage.clear();
+  document.getElementById("content").value = "";
+
+  const li = document.getElementsByClassName("tab");
+  //console.log(li);
+
+  while (li.length != 0) {
+    li[0].remove();
+  }
 });
